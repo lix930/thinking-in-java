2b6548e5087ed23fd6ee2cce3e8b3257687d865b
fish-xiang <lix930@163.com>
2016-09-08 17:13:07 +0800

    test

---
+++ test/src/HelloDate.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+/**
+ * Created by fishXiang on 2016/9/8.
+ */
+public class HelloDate {
+    public static void main(String[] args) {
+        System.out.println("hello, it's:");;
+        System.out.println(new Date());
+    }
+}
