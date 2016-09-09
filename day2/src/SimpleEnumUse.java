d0abd1fa21909010428d3e5a182a2d610193cf03
fish-xiang <lix930@163.com>
2016-09-09 21:47:00 +0800

    enum example

---
+++ day2/src/SimpleEnumUse.java
@@ -0,0 +1,9 @@
+/**
+ * Created by fish-Xiang on 2016/9/9.
+ */
+public class SimpleEnumUse {
+    public static void main(String[] args) {
+        Spiciness howHot = Spiciness.MEDIUM;
+        System.out.println(howHot);
+    }
+}
---
+++ day2/src/Spiciness.java
@@ -0,0 +1,6 @@
+/**
+ * Created by fish-Xiang on 2016/9/9.
+ */
+public enum Spiciness {
+    NOT, MILD, MEDIUM, HOT, FLAMING
+}
