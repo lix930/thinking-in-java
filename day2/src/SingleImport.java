fe8acdf8a4693720c335bf3068c43959e0e226ce
fish-xiang <lix930@163.com>
2016-09-09 22:44:33 +0800

    ArrayList

---
+++ day2/src/SingleImport.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+/**
+ * Created by fish-Xiang on 2016/9/9.
+ */
+public class SingleImport {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        list.add(3);
+
+        list.add(5);
+        System.out.println(list.toString());
+    }
+}
